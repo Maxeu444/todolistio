35b87c0e2aca9c5e7a93c5dac93d2f6db00cb659
Max H <139349788+Maxeu444@users.noreply.github.com>
2024-08-31 23:38:38 +0200

    Update next.config.mjs

--- todolist/next.config.mjs
+++ todolist/next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// next.config.mjs
+export default {
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Appliquez cette règle à toutes les routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
+};
+
