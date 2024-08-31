403d8b53bafc0d58091d2b4ceb2d7ebbbcbb46c6
Max H <139349788+Maxeu444@users.noreply.github.com>
2024-09-01 00:14:53 +0200

    Update route.ts cache params x2

--- todolist/app/api/task/all/route.ts
+++ todolist/app/api/task/all/route.ts
@@ -1,23 +1,29 @@
 import Task from "@models/tasks";
 import { connectToDB } from "@utils/database";
-
 import { NextResponse } from "next/server";
 
-export const GET = async(request: Request) => {
-    try {
-        await connectToDB()
+export const GET = async (request: Request) => {
+  try {
+    await connectToDB();
 
-        const tasks = await Task.find({})
+    const tasks = await Task.find({});
 
-        return NextResponse.json(
-            tasks,
-            { status: 200 }
-        )
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json(
-            "Failed to fetch all tasks",
-            { status: 500 }
-        )
-    }
-}
+    return new NextResponse(
+      JSON.stringify(tasks), 
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify("Failed to fetch all tasks"),
+      { status: 500 }
+    );
+  }
+};
